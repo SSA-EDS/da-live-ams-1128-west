c1d0c8e596d22a61980bda581de7f716f6cadb40
Alex Grimes <agrimes@adobe.com>
2026-07-31 17:55:09 -0600

    chore: remove IMS scope for aem.frontend.all

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -54,7 +54,7 @@ const CONFIG = {
   providers: { da: window.location.origin },
   decorateArea,
   imsClientId: 'ssa-eds',
-  imsScope: 'ab.manage,AdobeID,gnav,openid,org.read,read_organizations,session,additional_info.ownerOrg,additional_info.projectedProductContext,account_cluster.read,aem.frontend.all',
+  imsScope: 'ab.manage,AdobeID,gnav,openid,org.read,read_organizations,session,additional_info.ownerOrg,additional_info.projectedProductContext,account_cluster.read',
 };
 
 export default async function loadPage() {
